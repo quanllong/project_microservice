03d133284945f2fb98b21cb1917228c537675931
quanllong <quanllong_1@163.com>
2020-06-25 22:36:21 +0800

    清楚无用导入

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/promo/PromoController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/promo/PromoController.java
@@ -19,13 +19,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
